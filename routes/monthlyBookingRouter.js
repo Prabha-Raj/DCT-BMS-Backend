a36f4bc956a5ea8cf64476e10b9e144c77f12b71
Prabha-Raj <prabhakarrajput86300@gmail.com>
2025-09-23 16:59:25 +0530

    updated

--- routes/monthlyBookingRouter.js
+++ routes/monthlyBookingRouter.js
@@ -11,10 +11,9 @@ import { newCreateMonthlyBooking } from '../controllers/newMonthlyBookingControl
 
 const router = express.Router();
 
-router.use()
 
-router.post('/',protect, studentOnly, createMonthlyBooking);
 router.post('/create', newCreateMonthlyBooking);
+router.post('/',protect, studentOnly, createMonthlyBooking);
 router.get('/',protect, adminOnly, getMonthlyBookingsForAdmin);
 router.get('/student/my-booking',protect, studentOnly, getMyMonthlyBookings);
 router.get('/librarian/my-booking',protect, librarianOnly, getMonthlyBookingsForLibrarian);
